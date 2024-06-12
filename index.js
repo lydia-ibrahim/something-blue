4862dc19135100bb05c7c7b502f7186f83444320
lydia-ibrahim <lydiaibrahim@protonmail.com>
2024-06-12 10:55:13 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+function openModal() {
+ document.getElementById("myModal").style.display = "block";
+}
+
+
+function closeModal() {
+ document.getElementById("myModal").style.display = "none";
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+ showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+ showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+ var i;
+ var slides = document.getElementsByClassName("mySlides");
+ //var dots = document.getElementsByClassName("demo");
+ var captionText = document.getElementById("caption");
+ if (n > slides.length) {slideIndex = 1}
+ if (n < 1) {slideIndex = slides.length}
+ for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+ }
+ // for (i = 0; i < dots.length; i++) {
+ //     dots[i].className = dots[i].className.replace("active2", "");
+ // }
+ slides[slideIndex-1].style.display = "block";
+ // dots[slideIndex-1].className += "active2";
+ //captionText.innerHTML = "POXUI";//dots[slideIndex-1].alt;
+}
